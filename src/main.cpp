5af7d2fe0db8078e3c3531c4f67b2b6b65c35245
Jeroen <jeroenvanhattem@outlook.com>
2018-08-25 23:02:07 +0200

    Removed comments and added examples in the main loop

--- src/main.cpp
+++ src/main.cpp
@@ -7,30 +7,17 @@
 #include "robot_arm.hpp"
 #include <iostream>
 
-/*
-- *     Te weinig code
-- *     Sleep in constructor ipv main
-- *     commandDone() --> Blocking / Non-blocking versies van move(), closeClaw(), etc.
-- *     Check of hij klaar is met moven door de huidige coordinaten op te vragen en te vergelijken met de opgegeven coordinaten.
-- *     Dubbele if in command done; mogelijkheid "ok" te missen als deze op de grens van 2 slices ligt
-- *     readData via return value of niet?
-- *     createGCode kan betere naam hebben: maakt enkel speciale gCodes, en levert niets op. Was beter geweest als deze private is
-- *     Waarom creatGCode (of nog beter: move) niet hergebruiken voor de reset?
-- *     Isoleren GCode code goed idee (want duidelijk wat aangepast moet worden voor andere arm) maar niet consequent gedaan
-- *     Hergebruik je code: bijvoorbeeld, gebruik move in moveX (etc) - of vice versa
-- *     Delta moves waren mooie toevoeging geweest: niet alleen move naar X = 42, maar ook move 3 over X-as (moveDeltaX)
-- Tests missen compleet. Hardware tests zouden een goede toevoeging zijn.
-- *     Meer code / functionaliteit: moveSafe (check boundaries), bounaries aanpassen (bijvoorbeeld om een pen vast te kunnen
-houden), moveDeltaX/Y/Z, ... (wees creatief)
-- *     moveDelta()
-- *     moveSafe()
-- *     Fix serial voor Windows (receiving too many lines)
-*/
-
 int main() {
     RobotArm uArm = RobotArm();
+
     Coordinate3D coordinates = Coordinate3D(150, 150, 100);
     int speed = 50000;
+
+    std::cout << "RESETTING" << std::endl;
+
+    uArm.resetPosition();
+    while (!uArm.commandDone(1)) {
+    }
 
     uArm.move(coordinates, speed);
     while (!uArm.commandDone(1)) {
@@ -79,35 +66,35 @@ int main() {
         std::cout << "Z position unsuccesfull" << std::endl;
     }
 
-    // uArm.moveDeltaX(-10);
-    // while (!uArm.commandDone(1)) {
-    // }
+    uArm.moveDeltaX(-10);
+    while (!uArm.commandDone(1)) {
+    }
 
-    // if (uArm.getCurrentPosition() == uArm.getActualPosition()) {
-    //     std::cout << "Delta X position succesfull" << std::endl;
-    // } else {
-    //     std::cout << "Delta X position unsuccesfull" << std::endl;
-    // }
+    if (uArm.getCurrentPosition() == uArm.getActualPosition()) {
+        std::cout << "Delta X position succesfull" << std::endl;
+    } else {
+        std::cout << "Delta X position unsuccesfull" << std::endl;
+    }
 
-    // uArm.moveDeltaY(10);
-    // while (!uArm.commandDone(1)) {
-    // }
+    uArm.moveDeltaY(10);
+    while (!uArm.commandDone(1)) {
+    }
 
-    // if (uArm.getCurrentPosition() == uArm.getActualPosition()) {
-    //     std::cout << "Delta Y position succesfull" << std::endl;
-    // } else {
-    //     std::cout << "Delta Y position unsuccesfull" << std::endl;
-    // }
+    if (uArm.getCurrentPosition() == uArm.getActualPosition()) {
+        std::cout << "Delta Y position succesfull" << std::endl;
+    } else {
+        std::cout << "Delta Y position unsuccesfull" << std::endl;
+    }
 
-    // uArm.moveDeltaZ(10);
-    // while (!uArm.commandDone(1)) {
-    // }
+    uArm.moveDeltaZ(10);
+    while (!uArm.commandDone(1)) {
+    }
 
-    // if (uArm.getCurrentPosition() == uArm.getActualPosition()) {
-    //     std::cout << "Delta Z position succesfull" << std::endl;
-    // } else {
-    //     std::cout << "Delta Z position unsuccesfull" << std::endl;
-    // }
+    if (uArm.getCurrentPosition() == uArm.getActualPosition()) {
+        std::cout << "Delta Z position succesfull" << std::endl;
+    } else {
+        std::cout << "Delta Z position unsuccesfull" << std::endl;
+    }
 
     std::cout << "RESETTING" << std::endl;
 
@@ -115,29 +102,26 @@ int main() {
     while (!uArm.commandDone(1)) {
     }
 
-    if (uArm.getCurrentPosition() == uArm.getActualPosition()) {
-        std::cout << "Position reset succesfull" << std::endl;
-    } else {
-        std::cout << "Position reset unsuccesfull" << std::endl;
-    }
-
     uArm.rotateClaw(180);
+    std::cout << "Claw rotation: " << uArm.getClawRotation() << std::endl;
+
+    uArm.rotateClaw(0);
     std::cout << "Claw rotation: " << uArm.getClawRotation() << std::endl;
 
     uArm.closeClaw();
     uArm.mSleep(2000);
-    // std::cout << "Claw state: " << uArm.getClawState() << std::endl;
+    std::cout << "Claw state: " << uArm.getClawState() << std::endl;
 
     uArm.openClaw();
     uArm.mSleep(2000);
     std::cout << "Claw state: " << uArm.getClawState() << std::endl;
 
-    uArm.disablePump();
-    uArm.mSleep(100);
+    uArm.enablePump();
+    uArm.mSleep(1000);
     std::cout << "Pump state: " << uArm.pumpState() << std::endl;
 
     uArm.disablePump();
-    uArm.mSleep(100);
+    uArm.mSleep(1000);
     std::cout << "Pump state: " << uArm.pumpState() << std::endl;
 
     return 0;
